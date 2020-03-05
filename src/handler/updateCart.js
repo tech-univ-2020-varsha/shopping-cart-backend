d20b4434d76c6f8909dc3dd5b38ea04b4a93d6d1
Varsha C L <clvarsha1998@gmail.com>
2020-03-05 19:42:45 +0530

    fix:update cart route to prevent addition of item with zero quantity

--- src/handler/updateCart.js
+++ src/handler/updateCart.js
@@ -2,7 +2,8 @@ const dbOperations = require('../utils/dbOperations');
 
 const updateCartDetails = async (request, h) => {
   try {
-    const cartDetails = request.payload.products;
+    let cartDetails = request.payload.products;
+    cartDetails = cartDetails.filter((product) => product.quantity !== 0);
     const newCartDetails = cartDetails.map((product) => {
       const newProduct = product;
       newProduct.total = product.quantity * product.price;
